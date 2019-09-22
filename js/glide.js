4f8bb88604ece7f4bfb09af8235482bdbcd181fa
DZakh <dzakh.dev@gmail.com>
2019-09-23 00:46:01 +0300

    Beginning of work with new corousel Siema

--- js/glide.js
+++ js/glide.js
@@ -5,10 +5,13 @@ var glide = new Glide('.glide', {
 	type: 'carousel',
 	perView: 1,
 	gap: 0,
-	autoplay: 3250,
-	animationDuration: 700,
+	autoplay: 3500,
+	animationDuration: 350,
 	hoverpause: false,
 	keyboard: true,
-	animationTimingFunc: 'ease-out'
+  animationTimingFunc: 'ease-out',
+  dragThreshold: 80,
+  swipeThreshold: 60,
+  perTouch: 1
 })
 glide.mount()

f76563e69c66ab2be9824901e5bf20f8006657a9
DZakh <dzakh.dev@gmail.com>
2019-10-05 01:19:34 +0300

    Form with fetch (doesn't work)

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ let express =  require('express');
 let bodyParser = require('body-parser');
 let sender = require('./js/sender');
 let app = express();
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let urlencodedParser = bodyParser.urlencoded({ extended: true });
 let path = require('path')
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public_html'));
 app.use('/node_modules',express.static(path.join(__dirname, 'node_modules')));
-  app.use('/', bodyParser.urlencoded({
-      extended: true
-  }));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
   app.post('/', function(req, res, next) {
       if(!req.body)return res.sendStatus(400);
       console.log(req.body);
       let obj = req.body;
-      sender("Имя: " + obj.username + "\nНомер телефона: " + obj.tel);
+      sender("Имя: " + obj.user + "\nНомер телефона: " + obj.tel);
       // res.type('text/plain');
       // res.json(req.body);
-      // res.end();
+       res.end();
   });
 
   app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(dirname + "/public_html/index.html");
   });
 app.listen(5500);
+
+
+process.chdir('/home/a0318870/domains/dzakh.dev/public_html');

455a2911af3d4ebc7c025faa13071760a8d2663a
DZakh <dzakh.dev@gmail.com>
2019-10-04 12:52:33 +0300

    The beginning of work with a contact form

---
+++ app.js
@@ -0,0 +1,2 @@
+require('/home/a0318870/domains/dzakh.dev/public_html/index.js');
+process.chdir('/home/a0318870/domains/dzakh.dev/public_html');
---
+++ index.js
@@ -0,0 +1,58 @@
+let express =  require('express');
+let bodyParser = require('body-parser');
+//
+//
+let app = express();
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+
+function sender(text) {
+  let nodemailer = require('nodemailer');
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'send.company.bot@gmail.com',
+      pass: 'javaservelet'
+    }
+  });
+
+  let mailOptions = {
+    from: 'send.company.bot@gmail.com',
+    to: 'gabi.broun@gmail.com, dmirddz@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: text
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+  
+
+  app.use('/', bodyParser.urlencoded({
+      extended: true
+  }));
+
+  app.post('/', function(req, res, next) {
+      if(!req.body)return res.sendStatus(400);
+      console.log(req.body);
+      let obj = req.body;
+      sender("Имя: " + obj.username + "\nНомер телефона: " + obj.tel);
+      // res.type('text/plain');
+      // res.json(req.body);
+      // res.end();
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+
+app.listen(5500);

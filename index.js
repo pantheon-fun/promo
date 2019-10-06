d79de4e97aa853dd33f7ee325f01ccc2d817691f
DZakh <dzakh.dev@gmail.com>
2019-10-06 05:47:27 +0300

    REWORKED server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const sender = require('js/sender');
+const sender = require('./js/sender');
 const app = express();
 app.use(compression());
-// const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const path = require('path')
 
 app.use(express.static('public_html'));
@@ -14,21 +14,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-  app.post('/', function(req, res, next) {
-      if (!req.body)
-        return res.sendStatus(400);
-      console.log(req.body);
-      let obj = req.body;
-      sender("Имя: " + obj.user + "\nНомер телефона: " + obj.tel);
-      // res.type('text/plain');
-      // res.json(req.body);
-       res.end();
-  });
+app.post('/mail', function(req, res, next) {
+  if (!req.body)
+    return res.sendStatus(400);
+  console.log(req.body);
+  let obj = req.body;
+  sender("Имя: " + obj.user + "\nНомер телефона: " + obj.tel);
+  // res.type('text/plain');
+  // res.json(req.body);
+  res.status(201).send({
+    message: "Почта отправлена",
+    status: "ok"
+  })
+});
 
-  app.get('/', function(req, res) {
-    res.sendFile(dirname + "/public_html/index.html");
-  });
+app.get('/', function(req, res) {
+  res.sendFile(dirname + "/public_html/index.html");
+});
+
+app.get('/test', function(req, res) {
+  res.send({
+    pe: "wrewr"
+  })
+})
+
 app.listen(5500);
 
 
-process.chdir('/home/a0318870/domains/dzakh.dev/public_html');
+// process.chdir('/home/a0318870/domains/dzakh.dev/public_html');

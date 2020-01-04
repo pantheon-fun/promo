37153e592f20a9fbf6427b6fa3a86b12aee4ab6b
DZakh <dzakh.dev@gmail.com>
2020-01-04 14:12:03 +0300

    Fixing click on siema control arrows

--- src/js/packages/siema.js
+++ src/js/packages/siema.js
@@ -35,5 +35,17 @@ export const activateSiemaSlider = () => {
     arrow.addEventListener('touchstart', cancelAutoplay);
     arrow.addEventListener('mouseleave', reAutoplay);
     arrow.addEventListener('touchend', reAutoplay);
+    arrow.addEventListener('click', e => {
+      switch (e.target.dataset.siemaArrow) {
+        case 'next':
+          mySiema.next();
+          break;
+        case 'prev':
+          mySiema.prev();
+          break;
+        default:
+          throw new Error('Wrong dataset for siema control arrows.');
+      }
+    });
   });
 };

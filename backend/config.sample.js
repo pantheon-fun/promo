06e24a038d08b64f44b042aac90f972de5983189
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-01-19 11:46:20 +0300

    Fixing config sample

--- backend/config.sample.js
+++ backend/config.sample.js
@@ -1,27 +1,3 @@
-require('dotenv').config();
-
-module.exports.config = {
-  port: process.env.PORT,
-  sender: {
-    direct: true,
-    host: 'smtp.yandex.ru',
-    port: 465,
-    auth: {
-      user: 'YOUR_EMAIL',
-      pass: 'YOUR_PASS',
-    },
-    secure: true,
-  },
-  mailOptions: {
-    from: 'YOUR_EMAIL',
-    to: 'some@email.com, another@email.com',
-    subject: 'Запрос звонка!',
-    text: null,
-  },
-  vkAdmins: ['YOUR_VK_ADMINS'],
-  vkToken: 'YOUR_VK_TOKEN',
-};
-
 module.exports.config = {
   development: {
     port: process.env.PORT || 3000,

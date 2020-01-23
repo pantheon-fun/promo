f113d6d7965e1baa7c5f57f102b9bb605f1d6a86
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-01-23 23:50:28 +0300

    Messing around

--- src/components/contacts/contacts.component.jsx
+++ src/components/contacts/contacts.component.jsx
@@ -3,24 +3,20 @@ import React from 'react';
 
 import Styles from './contacts.module.scss';
 
-export const Contacts = ({ className }) => {
-  console.log(Styles);
-
-  return (
-    <div className={`${className} ${Styles.contacts}`}>
-      <button data-smooth-scroll-to="references-section" className={Styles.address}>
-        Олимпийский проспект 20к1, Мытищи
-      </button>
-      <br />
-      <div className={Styles.tel}>
-        <i className={`icon-phone ${Styles.telIcon}`}></i>
-        <a href="tel:+79777220960" className={Styles.telNumber}>
-          +7 977 722 09 60
-        </a>
-      </div>
+export const Contacts = ({ className }) => (
+  <div className={`${className} ${Styles.contacts}`}>
+    <button data-smooth-scroll-to="references-section" className={Styles.address}>
+      Олимпийский проспект 20к1, Мытищи
+    </button>
+    <br />
+    <div className={Styles.tel}>
+      <i className={`icon-phone ${Styles.telIcon}`}></i>
+      <a href="tel:+79777220960" className={Styles.telNumber}>
+        +7 977 722 09 60
+      </a>
     </div>
-  );
-};
+  </div>
+);
 
 Contacts.propTypes = {
   className: PropTypes.string,

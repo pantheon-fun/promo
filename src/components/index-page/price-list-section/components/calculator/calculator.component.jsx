58ec7628663686f4690df135cce1afc4608c7878
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-03-29 19:30:32 +0300

    Finishing calculator component

--- src/components/index-page/price-list-section/components/calculator/calculator.component.jsx
+++ src/components/index-page/price-list-section/components/calculator/calculator.component.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
+import { useInView } from 'react-intersection-observer';
 
 import Styles from './calculator.module.scss';
 
 import { Spinner } from '../../../../common/spinner';
 
+import { CALC_WIDGET_ID, WIDGET_OBERVER_OPTIONS } from '../../../../../utils/constants';
+
+const loadCalculator = () => {
+  const calcScript = document.createElement('script');
+
+  calcScript.async = true;
+  calcScript.src = `${
+    window.location.protocol === 'https:' ? 'https:' : 'http:'
+  }//ucalc.pro/api/widget.js?id=${CALC_WIDGET_ID}&t=${Math.floor(new Date() / 18e5)}`;
+
+  document.documentElement.appendChild(calcScript);
+
+  return <div className={`uCalc_${CALC_WIDGET_ID}`} />;
+};
+
 const Calculator = ({ className }) => {
+  const [ref, inView] = useInView(WIDGET_OBERVER_OPTIONS);
+  const [widgetContent, setWidgetContent] = useState(<Spinner />);
+
+  useEffect(() => {
+    if (inView === true) {
+      setWidgetContent(loadCalculator());
+    }
+  }, [inView]);
+
   return (
-    <div className={`${className} ${Styles.container}`}>
-      <div className={Styles.widget} id="calculator">
-        <Spinner />
-      </div>
+    <div ref={ref} className={cn(className, Styles.container)}>
+      <div className={Styles.widget}>{widgetContent}</div>
     </div>
   );
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const WIDGET_OBERVER_OPTIONS = {
+  rootMargin: '20% 0px 75% 0px',
+  triggerOnce: true,
+};
+
+export const CALC_WIDGET_ID = 231405;
